9d549f80b8d57ca367dba6171f0bac355c173cf3
JC Zaragoza <jzara3115@gmail.com>
2024-10-23 13:02:35 -0700

    Step 1 complete - added canvas and border

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import "./style.css";
 
-const APP_NAME = "BARELT STARTED?";
+const APP_NAME = "BARELY STARTED?";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `${APP_NAME}`;
+
+const canvas = document.createElement('canvas') as HTMLCanvasElement;
+canvas.id = 'myCanvas';
+app.appendChild(canvas);
+
